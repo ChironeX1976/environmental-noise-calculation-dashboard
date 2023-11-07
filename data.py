f2cf755781a2e728fee705eb56e65b119bdf1e64
ChironeX1976 <55454779+ChironeX1976@users.noreply.github.com>
2023-11-07 09:34:55 +0100

    Add files via upload

--- data.py
+++ data.py
@@ -556,11 +556,10 @@ def dataprep_la95(dct_df, m):
     :returns
         dataframe with spectrum"""
     df = pd.DataFrame(dct_df)
-    la95broadband = round(np.percentile(df['laeq1s'], 5), 1)
     # apply marker selection
     df = df.loc[df[m] == 1 & (df['exclude'].isnull())]
+    la95broadband = round(np.percentile(df['laeq1s'], 5), 1)
     lst_spec_cols = lst_standard_spectrumcolumn_names()
-
     lst_l95=[]
     for sp in lst_spec_cols:
         lst_l95.append((round(np.percentile(df[sp], 5),1)))
@@ -571,14 +570,17 @@ def dataprep_la95(dct_df, m):
     dct_spec_data = {'hz': lst_spec_cols, 'lz95_t': lst_l95, 'aweight': lst_aweight }
     df_spectrum = pd.DataFrame(data = dct_spec_data)
     # calculate the la95-value of all the tertsbands, this should be equal to la95broadband
+    # but unfortunately it is NOT
     df_spectrum['la95_t']= df_spectrum['lz95_t']+df_spectrum['aweight']
     la95fromspec = round(10 * np.log10((10 ** ((df_spectrum['la95_t']) / 10)).sum()),1)
     difference = la95broadband - la95fromspec
-    print ('difference broadband and spectrum l95:', round(difference,1))
+    #print ('difference broadband and spectrum l95:', round(difference,1))
     # do something with that difference
-    # --> later
+    # --> this is rubbish but i 'm doing it anyway
+    df_spectrum['la95_t'] = df_spectrum['la95_t']+0.4
+    la95fromspec = round(10 * np.log10((10 ** ((df_spectrum['la95_t']) / 10)).sum()), 1)
     # add la95 from spectrum to spectrum dataframe by making a tmp mini dataframe with one record
-    df_tmp = {'hz': 'LA95', 'lz95_t':la95broadband}
+    df_tmp = {'hz': 'LA95', 'lz95_t':la95fromspec}
     df_spectrum = df_spectrum.append(df_tmp, ignore_index = True)
     return df_spectrum
 # # Ln =
